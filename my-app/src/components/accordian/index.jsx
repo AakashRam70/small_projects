d10ac1ffb0315bc24559ba0789c821a5993a8423
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-04 23:19:02 +0530

    updating accordian as multiwork function

--- my-app/src/components/accordian/index.jsx
+++ my-app/src/components/accordian/index.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import data from './data';
+import './styles.css'
 
 const Accordian = () => {
 
     const [selected, setSelected] = useState(null)
 
     function handleOne(getCurrentId) {
-        setSelected(getCurrentId);
+        setSelected(getCurrentId === selected ? null : getCurrentId);
     }
 
     return (
-        <div className='merging'>
+        <div className='wrapper'>
+            <button>Enable Multi Selection</button>
             <div className="accordian">
                 {
                     data && data.length > 0 ?
@@ -19,6 +21,8 @@ const Accordian = () => {
                                 <div onClick={() => handleOne(dataItem.id)} className='title'>
                                     <h3>{dataItem.questions}</h3>
                                     <span>+</span>
+                                </div>
+                                <div className='content'>
                                     {
                                         selected === dataItem.id ? <div>{dataItem.answer}</div> : null
                                     }
