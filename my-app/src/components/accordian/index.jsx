9f8531323bdf8c26c5bc1d842c2edbc61ee3cdd0
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-04 23:53:48 +0530

    updating accordian as multiwork function

--- my-app/src/components/accordian/index.jsx
+++ my-app/src/components/accordian/index.jsx
@@ -5,20 +5,38 @@ import './styles.css'
 const Accordian = () => {
 
     const [selected, setSelected] = useState(null)
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+    const [multiple, setMultiple] = useState([])
 
-    function handleOne(getCurrentId) {
+    function handleSingleSelection(getCurrentId) {
         setSelected(getCurrentId === selected ? null : getCurrentId);
     }
 
+    function handleMultiSelection(getCurrentId) {
+        let copystate = [...multiple];
+        const findIndexofCurrentId
+            = copystate.indexOf(getCurrentId);
+
+        if (findIndexofCurrentId === -1) copystate.push(getCurrentId)
+        else copystate.splice(findIndexofCurrentId, 1)
+
+        setMultiple(copystate);
+    }
+
+    console.log(selected, multiple)
+
     return (
         <div className='wrapper'>
-            <button>Enable Multi Selection</button>
+            <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
             <div className="accordian">
                 {
                     data && data.length > 0 ?
                         data.map(dataItem => (
                             <div className='item'>
-                                <div onClick={() => handleOne(dataItem.id)} className='title'>
+                                <div onClick={handleMultiSelection ? () =>
+
+                                    handleMultiSelection(dataItem.id) :
+                                    handleSingleSelection(dataItem.id)} className='title'>
                                     <h3>{dataItem.questions}</h3>
                                     <span>+</span>
                                 </div>
