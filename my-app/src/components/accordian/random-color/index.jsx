b1291b80fd65e255a31c0fd9510e6192cd10e886
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-15 00:07:48 +0530

    random_color

--- my-app/src/components/accordian/random-color/index.jsx
+++ my-app/src/components/accordian/random-color/index.jsx
@@ -5,6 +5,25 @@ export default function RandomColor() {
     const [typeOfColor, setTypeOfColor] = useState('hex');
     const [color, setColor] = useState("black");
 
+    function randomColorUtility(length) {
+        return Math.floor(Math.random() * length)
+    }
+
+    function handleCreateRandomHexColor() {
+        const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "c", "D", "E", "F"]
+        let hexColor = "#";
+
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+
+        setColor(hexColor);
+    }
+
+    function handleCreateRandomRgbColor() {
+
+    }
+
     return (
         <div style={{
             width: '100vw',
@@ -13,7 +32,7 @@ export default function RandomColor() {
         }}>
             <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
             <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
-            <button>Generate Random Color</button>
+            <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
         </div>
     );
 }
