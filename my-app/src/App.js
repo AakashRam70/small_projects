462f07bef448d2992c693a3899f7ad3bfd3a5c78
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-22 00:54:18 +0530

    adding image-slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Accordion from './components/accordian';
-import RandomColor from './components/accordian/random-color';
-import StarRating from './components/accordian/star-rating';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
---
+++ my-app/src/components/image-slider/index.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ImageSlider({ url, limit }) {
+    return (
+        <div className="container">
+
+        </div>
+    )
+}
