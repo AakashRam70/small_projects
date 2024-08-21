d2e199a665b410bf2263131378bccf91b91f5fb1
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-22 00:46:40 +0530

    updating start-pattern

--- my-app/src/components/accordian/star-rating/index.jsx
+++ my-app/src/components/accordian/star-rating/index.jsx
@@ -1,39 +1,40 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import './styles.css'
 
-// import { useState } from "react"
-import { FaStar } from "react-icons/fa"
-export default function starRating({ noOfStars = 5 }) {
+export default function StarRating({ noOfStars = 5 }) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
 
-    // const [rating, setRating] = useState(0)
-    // const [hover, setHover] = useState(0)
-
-    function handleOnClick(getCurrent) {
-
+    function handleClick(getCurrent) {
+        setRating(getCurrent);
     }
+
     function handleMouseEnter(getCurrent) {
-
+        setHover(getCurrent);
     }
 
-    function handleMouseLeave(getCurrent) {
-
+    function handleMouseLeave() {
+        setHover(rating);
     }
 
     return (
         <div className="star-rating">
             {
                 [...Array(noOfStars)].map((_, index) => {
-
                     index += 1;
-
-                    return
-                    <FaStar
-                        key={index}
-                        onClick={() => handleOnClick(index)}
-                        onMouseMove={() => handleMouseEnter(index)}
-                        onMouseLeave={() => handleMouseLeave(index)}
-                        size={40}
-                    />
+                    return (
+                        <FaStar
+                            key={index}
+                            className={index <= (hover || rating) ? 'active' : 'inactive'}
+                            onClick={() => handleClick(index)}
+                            onMouseMove={() => handleMouseEnter(index)}
+                            onMouseLeave={() => handleMouseLeave()}
+                            size={40}
+                        />
+                    );
                 })
             }
         </div>
-    )
+    );
 }
